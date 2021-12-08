82fe5749b5f7ed122c4aa2f5201e1e782a71994a
Christian <ramoschristian39@gmail.com>
2021-12-07 20:59:24 -0400

    MainMenu tweaks.

--- src/Clue/MainMenu.java
+++ src/Clue/MainMenu.java
@@ -1,9 +1,8 @@
-package Clue;
+package clue;
 
 import java.awt.*;
 import java.awt.event.*;
 import java.util.Arrays;
-import java.util.Iterator;
 
 import javax.swing.*;
 
@@ -12,48 +11,80 @@ public class MainMenu extends JPanel {
 	static final int SCREEN_WIDTH = 842;
 	static final int SCREEN_HEIGHT = 872;
 	JFrame frame = new JFrame();
-
-	// Gotta work on moving the Color enum alone.
-
-	// Starts for every player to either start a game or join one.
-	// This main menu must contain every color available for the user to choose.
-	// Eliminates the chosen colors for the next client...
+	
 
 	// Waits for every player to choose a color and join the game,
 	// then every player gets the same gameboard with every location for
 	// their chosen color.
 	public MainMenu() {
 
+		this.setLayout(new GridBagLayout());
+		//Adds the game buttons
 		addGameButtons();
-
-		this.setPreferredSize(new Dimension(SCREEN_WIDTH / 3, SCREEN_HEIGHT / 7));
-
 		this.setFocusable(true);
+		//setLocation to center of screen
 		frame.setLocationRelativeTo(null);
+		//ImageIcon
+		ImageIcon logo = new ImageIcon("C:\\Users\\djavi\\Desktop\\iconImage.png");
+		frame.setIconImage(logo.getImage());
 		frame.add(this);
 		frame.setTitle("Clue");
 		frame.setResizable(false);
 		frame.pack();
 		frame.setVisible(true);
 
-	}// constructor
+	}//constructor
+	
 
+	//Adds Start button or Join button
 	private void addGameButtons() {
-
-		JButton startGameBTN = new JButton("Start Game");
-
+		
+		GridBagConstraints c = new GridBagConstraints();
+		c.gridx = 0;
+		c.gridy = 0;
+		
+		GridBagConstraints c01 = new GridBagConstraints();
+		c01.gridx = 0;
+		c01.gridy = 1;
+		
+		GridBagConstraints c11 = new GridBagConstraints();
+		c11.gridx = 1;
+		c11.gridy = 1;
+		
+		GridBagConstraints c02 = new GridBagConstraints();
+		c02.gridx = 0;
+		c02.gridy = 2;
+		
+		//Input text from user
 		JLabel enterText = new JLabel("Entre la cantidad de jugadores (2-6): ");
-
 		JTextField text = new JTextField(5);
 
-		JButton submitBtn = new JButton("Submit");
+		//Author's Note
+		JLabel authorsNote = new JLabel("Created by Gabriel Rodriguez, Claudia Monge, and Christian Ramos. December 2021. ");
+		authorsNote.setFont(new Font("Serif", Font.PLAIN, 10));
+		
+		//Menu Image
+		ImageIcon icon = new ImageIcon("C:\\Users\\djavi\\Desktop\\menu.png");//change path
+		JLabel img = new JLabel();
+		img.setIcon(icon);
+		
 
-		submitBtn.addActionListener(new ActionListener() {
+		//Once client opens MainMenu, start the client thread
+		Client client = new Client();
+		Thread thread = new Thread(client);
+		thread.start();
+
+		//Submit Button
+		JButton submitBtn = new JButton("Submit");
+		submitBtn.setFont(new Font("Serif", Font.BOLD, 15));
+
+
+		submitBtn.addActionListener(new ActionListener(){
 
 			public void actionPerformed(ActionEvent e) {
 
 				Client.amountofPlayers = Integer.parseInt(text.getText());
-
+				
 				enterText.setVisible(false);
 
 				text.setVisible(false);
@@ -66,22 +97,20 @@ public class MainMenu extends JPanel {
 
 		});
 
-		JButton joinGameBTN = new JButton("Join Game");
-
-		startGameBTN.setSize(350, 350);
-
+		//Start Button
+		JButton startGameBTN = new JButton("Start Game");
+		startGameBTN.setFont(new Font("Serif", Font.BOLD, 15));
 		startGameBTN.addActionListener(new ActionListener() {
 
 			public void actionPerformed(ActionEvent e) {
-
-				Client client = new Client();
-				Thread thread = new Thread(client);
-				thread.start();
+				
+				img.setVisible(false);
+				
+				authorsNote.setVisible(false);
 
 				startGameBTN.setVisible(false);
-				joinGameBTN.setVisible(false);
 
-				// We need to gather the amount of players for the first player
+				//We need to gather the amount of players for the first player
 				enterText.setVisible(true);
 				text.setVisible(true);
 				submitBtn.setVisible(true);
@@ -90,53 +119,76 @@ public class MainMenu extends JPanel {
 
 		});
 
-		joinGameBTN.setSize(350, 350);
+		//Join Button
+		JButton joinGameBTN = new JButton("Join Game");
+		joinGameBTN.setFont(new Font("Serif", Font.BOLD, 15));
 
+		
 		joinGameBTN.addActionListener(new ActionListener() {
 
 			public void actionPerformed(ActionEvent e) {
-
-				Client client = new Client();
-				Thread thread = new Thread(client);
-				thread.start();
-				startGameBTN.setVisible(false);
+				
+				img.setVisible(false);
+				
+				authorsNote.setVisible(false);
+				
 				joinGameBTN.setVisible(false);
+				
 				addColorButtons();
 
 			}
 
 		});
 
-		this.add(startGameBTN);
-		this.add(joinGameBTN);
+		this.add(img, c);
+		
+		//Show only the start button for the first client,
+		// the rest only join the game.
+		if(Client.currTurn == 0) 
+			this.add(startGameBTN,c01);
+		else 
+			this.add(joinGameBTN,c01);
+		
+		this.add(authorsNote, c02);
+
 		enterText.setVisible(false);
-		this.add(enterText);
+		this.add(enterText, c);
+
 		text.setVisible(false);
-		this.add(text);
+		this.add(text, c01);
+
 		submitBtn.setVisible(false);
-		this.add(submitBtn);
+		this.add(submitBtn, c11);
 
 	}
 
+	//Adds buttons to choose color
 	private void addColorButtons() {
-
+				
 		JButton greenColor = new JButton("Green");
+		greenColor.setFont(new Font("Serif", Font.BOLD, 20));
 		greenColor.setBackground(Colors.Green.getColor());
 
 		JButton plumColor = new JButton("Plum");
+		plumColor.setFont(new Font("Serif", Font.BOLD, 20));
 		plumColor.setBackground(Colors.Plum.getColor());
 
 		JButton scarlettColor = new JButton("Scarlett");
+		scarlettColor.setFont(new Font("Serif", Font.BOLD, 20));
 		scarlettColor.setBackground(Colors.Scarlett.getColor());
 
 		JButton orchidColor = new JButton("Orchid");
+		orchidColor.setFont(new Font("Serif", Font.BOLD, 20));
 		orchidColor.setBackground(Colors.Orchid.getColor());
 
 		JButton peacockColor = new JButton("Peacock");
+		peacockColor.setFont(new Font("Serif", Font.BOLD, 20));
 		peacockColor.setBackground(Colors.Peacock.getColor());
 
 		JButton mustardColor = new JButton("Mustard");
+		mustardColor.setFont(new Font("Serif", Font.BOLD, 20));
 		mustardColor.setBackground(Colors.Mustard.getColor());
+
 
 		greenColor.addActionListener(new ActionListener() {
 
@@ -152,13 +204,6 @@ public class MainMenu extends JPanel {
 
 			public void actionPerformed(ActionEvent e) {
 				Client.colorIdx = Arrays.binarySearch(Client.availableColors, "Plum");
-				greenColor.setVisible(false);
-				plumColor.setVisible(false);
-				orchidColor.setVisible(false);
-				mustardColor.setVisible(false);
-				greenColor.setVisible(false);
-				scarlettColor.setVisible(false);
-				peacockColor.setVisible(false);
 				frame.dispose();
 
 			}
@@ -205,41 +250,43 @@ public class MainMenu extends JPanel {
 
 		});
 
+		//Adds to the panel only the available colors
 		for (int i = 0; i < Client.availableColors.length; i++) {
 
 			switch (Client.availableColors[i]) {
-				case "Green": {
-					this.add(greenColor);
-					break;
-				}
-				case "Plum": {
-					this.add(plumColor);
-					break;
+			case "Green": 
+				this.add(greenColor);
+				break;
+			
+			case "Plum":
+				this.add(plumColor);
+				break;
+			
+			case "Orchid": 
+				this.add(orchidColor);
+				break;
+			
+			case "Scarlett": 
+				this.add(scarlettColor);
+				break;
+			
+			case "Peacock": 
+				this.add(peacockColor);
+				break;
+			
+			case "Mustard": 
+				this.add(mustardColor);
+				break;
+			
+			default:
+				throw new IllegalArgumentException("Unexpected value: " + Client.availableColors[i]);
+			}//switch
 
-				}
-				case "Orchid": {
-					this.add(orchidColor);
-					break;
-				}
-				case "Scarlett": {
-					this.add(scarlettColor);
-					break;
-				}
-				case "Peacock": {
-					this.add(peacockColor);
-					break;
-				}
-				case "Mustard": {
-					this.add(mustardColor);
-					break;
-				}
 
-				default:
-					throw new IllegalArgumentException("Unexpected value: " + Client.availableColors[i]);
-			}
+		}//for
+		
+	}//addColorButtons
 
-		}
 
-	}
 
 }
